62b2b7f1f962224d2e1977aa3de2c3a87b770094
Raoul Brigola <49130815+brigoraoul@users.noreply.github.com>
2021-04-25 17:54:03 +0200

    Update Blockchain.java

--- Blockchain.java
+++ Blockchain.java
@@ -2,35 +2,41 @@ package simple;
 
 import java.util.ArrayList;
 
-public class Blockchain {
+public class Blockchain
+{
 	
 	public ArrayList<Block> blocks = new ArrayList();
-	public int last_hash;
+	public int last_hash = 0;
 	
-	public Blockchain(Datapoints[] data) {
-		this.last_hash = 0;
-		Block genesis = new Block(data, last_hash);
-		this.blocks.add(genesis);
+	public Blockchain(Datapoints[] data)
+	{
+		/**
+		Adding genesis block upon creation of the blockchain
+		*/
+		add_block(data);
 	}
 	
-	public void add_block(Datapoints[] data) {
+	public void add_block(Datapoints[] data)
+	{
 		Block next = new Block(data, last_hash);
 		blocks.add(next);
 		this.last_hash = next.get_hash();
 	}
 	
-	private class Block {
-		
+	private class Block 
+	{	
 		public Datapoints[] data;
 		public int hash;
 		
-		public Block(Datapoints[] data, int previous_hash) {
+		public Block(Datapoints[] data, int previous_hash) 
+		{
 			this.data = data;
 			int[] to_hash = {data.hashCode(), previous_hash};
 			this.hash = to_hash.hashCode();
 		}
 		
-		public int get_hash() {
+		public int get_hash() 
+		{
 			return hash;
 		}
 		
